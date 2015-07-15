67a9acac6fc606b8c1e2aa23680b0335d17befa5
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-07-15 21:02:22 +0200

    styling - waiting for issue #1008
    Adds an extra textarea to be able to provide a style (OpenLayers JS
    object format).

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ GEOR.Addons.Osm2Geor = Ext.extend(GEOR.Addons.Base, {
     win: null,
     layer: null,
     item: null,
-    _textArea: null,
+    _queryTextArea: null,
+    _styleTextArea: null,
     _keepPreviousFeatures: null,
 
     init: function(record) {
@@ -21,8 +22,10 @@ GEOR.Addons.Osm2Geor = Ext.extend(GEOR.Addons.Base, {
     },
 
     createWindow: function() {
-        this._textArea = new Ext.form.TextArea({
+        this._queryTextArea = new Ext.form.TextArea({
             name       : 'overpassApiQuery',
+            width      : 375,
+            height     : 150,
             fieldLabel : 'Overpass API query',
             value      : '[out:json][timeout:25];(        \n \
                             node["highway"]{{BBOX}};      \n \
@@ -30,22 +33,34 @@ GEOR.Addons.Osm2Geor = Ext.extend(GEOR.Addons.Base, {
                           );                              \n \
                           out body;                       \n \
                           >;                              \n \
-                          out skel qt;',
+                          out skel qt;'
+        });
+        this._styleTextArea = new Ext.form.TextArea({
+            name       : 'olStyle',
+            fieldLabel : 'OpenLayers style',
+            value      : '{                                   \
+                "strokeWidth": 2,                             \
+                "strokeColor": "#ddd",                        \
+                "fillColor": "#ddd"                           \
+              }',
+            width      : 375,
+            height     : 150
         });
         this._keepPreviousFeatures = new Ext.form.Checkbox({
            boxLabel   : 'Keep previously loaded features',
            checked    : false
         });
+
         return new Ext.Window({
             closable: true,
             closeAction: 'hide',
             width: 500,
-            height: 270,
+            height: 400,
             title: "OSM 2 geOrchestra",
             border: false,
             buttonAlign: 'left',
-            layout: 'fit',
-            items: [ this._textArea ],
+            layout: 'form',
+            items: [ this._queryTextArea, this._styleTextArea ],
             listeners: {
                 'show': function() {
                     if (OpenLayers.Util.indexOf(this.map.layers, this.layer) < 0) {
@@ -58,30 +73,38 @@ GEOR.Addons.Osm2Geor = Ext.extend(GEOR.Addons.Base, {
             fbar: ['->', this._keepPreviousFeatures, {
                 text: OpenLayers.i18n("Execute"),
                 handler: function() {
-                	var ex = this.map.getExtent().transform(this.map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
-                	var query = this._textArea.getValue();
-                	query = query.replace(/{{BBOX}}/g, '(' + ex.bottom +',' +ex.left + ',' + ex.top +',' + ex.right +')');
-                	Ext.Ajax.request({
+                        var ex = this.map.getExtent().transform(this.map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
+                        var query = this._queryTextArea.getValue();
+                        query = query.replace(/{{BBOX}}/g, '(' + ex.bottom +',' +ex.left + ',' + ex.top +',' + ex.right +')');
+                        Ext.Ajax.request({
                         scope: this,
-                	    url: '/mapfishapp/ws/osm2geor/q',
-                	    method: 'POST',          
-                	    params: {
-                	        data: query
-                	    },
-                	    success: function(response) {
-                                var features = (new OpenLayers.Format.GeoJSON({
-                                   externalProjection: new OpenLayers.Projection("EPSG:4326"),
-                                   internalProjection: this.map.getProjectionObject() 
-                                })).read(response.responseText);
-                                if (this._keepPreviousFeatures.checked == false) {
-                                    this.layer.removeAllFeatures();
-                                }
-                                this.layer.addFeatures(features);
-                            },
-                	    failure: function() {
-                	    	alert('failure');
-                	    }
-                	});
+                        url: '/mapfishapp/ws/osm2geor/q',
+                        method: 'POST',
+                        params: {
+                            data: query
+                        },
+                        success: function(response) {
+                            if (this._styleTextArea.getValue() != "") {
+                                var jsStyle = new OpenLayers.Format.JSON({}).read(this._styleTextArea.getValue());
+                                var customStyle = new OpenLayers.Style(jsStyle);
+                                this.layer.styleMap = new OpenLayers.StyleMap(customStyle);
+                                this.layer.redraw();
+                            } else {
+                                this.layer.styleMap = new OpenLayers.StyleMap();
+                            }
+                            var features = (new OpenLayers.Format.GeoJSON({
+                                externalProjection: new OpenLayers.Projection("EPSG:4326"),
+                                internalProjection: this.map.getProjectionObject()
+                            })).read(response.responseText);
+                            if (this._keepPreviousFeatures.checked == false) {
+                                this.layer.removeAllFeatures();
+                            }
+                            this.layer.addFeatures(features);
+                        },
+                        failure: function() {
+                            alert('failure');
+                        }
+                    });
                 },
                 scope:this
             },
@@ -107,7 +130,6 @@ GEOR.Addons.Osm2Geor = Ext.extend(GEOR.Addons.Base, {
         this.layer = null;
         this.jsonFormat = null;
         this.modifyControl = null;
-        
         GEOR.Addons.Base.prototype.destroy.call(this);
     }
 });
